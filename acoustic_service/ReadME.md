19b2a5026a4eadf094f92cb0db78b0d83c3d07f4
sokanaid <sokanaid@mail.ru>
2022-05-02 11:10:59 +0000

    readme

--- acoustic_service/ReadME.md
+++ acoustic_service/ReadME.md
@@ -1,1 +1,21 @@
+#Acoustic_service
 
+    cd ./acoustic_service/.
+
+## To launch acoustic service local:
+    flask run  
+or
+    python3 app.py
+
+## To launch acoustic service in Docker:
+### Build container
+    docker build . -t fastpitch:latest  
+### Start container
+    docker run --gpus=all --name acoustic_service -e CUDA_VISIBLE_DEVICES -it --ipc=host -p 5000:5000 fastpitch:latest
+### Stop container
+    docker stop acoustic_service
+### remove container 
+    docker rm acoustic_service 
+
+## Example of requests:
+    python3 check.py
--- acoustic_service/check.py
+++ acoustic_service/check.py
@@ -7,12 +7,18 @@ import torch
 import os
 
 ans = requests.get('http://127.0.0.1:5000/?text=Hello+I+am+text+And+it+works')
-host = "http://127.0.0.1:8000"
-resp = requests.get(host)
-#print(np.array(ans.json()).T.shape)
-x = torch.from_numpy(np.array(ans.json()).T).unsqueeze(0)
-print(x.shape)
-obj = x.tolist()
-resp = requests.post(host, json=x.tolist())
-print(resp.status_code)
-open('out.wav', 'wb').write(resp.content)
+print("request to acoustic status:", ans.status_code)
+print("Try connect to vocoder")
+try:
+    host = "http://127.0.0.1:8000"
+    resp = requests.get(host)
+    #print(np.array(ans.json()).T.shape)
+    x = torch.from_numpy(np.array(ans.json()).T).unsqueeze(0)
+    print(x.shape)
+    obj = x.tolist()
+    resp = requests.post(host, json=x.tolist())
+    print("request to vocoder status:", resp.status_code)
+    open('out.wav', 'wb').write(resp.content)
+    print("audio saved to out.wav")
+except:
+    print("Can't connect to vocoder")
